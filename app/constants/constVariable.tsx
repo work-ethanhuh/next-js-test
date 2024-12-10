9945a2f6b6d8330ee87cc40d39420e179f6664a8
TUF-FX707ZV4\pable <htm@kiins.co.kr>
2024-12-10 11:59:48 +0900

    - 내용 변경

--- app/constants/constVariable.tsx
+++ app/constants/constVariable.tsx
@@ -2,361 +2,318 @@ import kakaoQR from "../assets/img/kakao_qr.png";
 
 export const kakaoQr = kakaoQR;
 export const headerNavigation = [
-    {
-        title: "이력사항",
-        url: "history"
-    },
-    {
-        title: "포트폴리오 요청",
-        url: "portfolio"
-    },
-    {
-        title: "요약",
-        url: "summary"
-    },
+  {
+    title: "이력사항",
+    url: "history",
+  },
+  {
+    title: "포트폴리오 요청",
+    url: "portfolio",
+  },
+  {
+    title: "요약",
+    url: "summary",
+  },
 ];
 
 export const historyText = [
-    {
-        yearmonth: "2024.05 - 현재",
-        text: ["키인즈", "서비스 개발팀", "선임연구원"],
-        title: [
-            "- U+ 화물잇고-차주앱 (Flutter - iOS/Android 앱) : 5월 ~ 9월",
-            "- 미래엔 관리자 웹 (NextJS) : 10월 ~ 현재",
-        ],
-        code: "",
-        view: "",
-        info: [
-            "Flutter",
-            "NextJS",
-            "Android",
-            "iOS",
-        ],
-    },
-    {
-        yearmonth: "2023.05 - 2024.01",
-        text: ["후이즈시스템", "솔루션 개발팀", "팀장"],
-        title: [
-            "- G2Works / G2Talk 전체 시스템 설계",
-            "- G2Works(그룹웨어) ( Flutter 앱 - Android / iOS )",
-            "- G2Talk(메신저) ( Flutter 앱 - Android / iOS / MacOS / Windows )",
-            "- G2Wokrs / G2Talk API ( Java Spring Boot )"
-        ],
-        code: "",
-        view: "임금 체불로 퇴사",
-        info: [
-            "Flutter - Android/iOS/Windows/MacOS",
-            "Java Spring Boot",
-            "Spring Data JDBC",
-            "Rest api",
-            "Docker",
-        ],
-    },
-    {
-        yearmonth: "2022.10 - 2023.02",
-        text: ["TDI", "프리랜서"],
-        title: ["- 눈팅 : 여성 커뮤니티 ( Flutter 앱 - Android / iOS )"],
-        code: "",
-        view: "",
-        info: [
-            "Flutter",
-            "Android",
-            "iOS",
-            "FastLane",
-        ],
-    },
-    {
-        yearmonth: "2021.03 - 2022.10",
-        text: ["UpTo", "개발총괄"],
-        title: [
-            "- B2B 기업 홍보 및 연결 앱 ( Flutter 앱 - Android / iOS )", 
-            "- API 서버 개발( Python Flask, MariaDB )",
-        ],
-        code: "",
-        view: "",
-        info: [
-            "Flutter",
-            "Android",
-            "iOS",
-            "Python Flask",
-            "MariaDB",
-            "Rest api",            
-        ],
-    },
-    {
-        yearmonth: "2020.07 - 2021.02",
-        text: ["파낙토스", "S/W팀", "과장"],
-        title: [
-            "- Panaxtos Manager 블루투스 뇌파 수신 어플 개발",
-            "- 사내 기존 C++ 관련 프로그램 유지보수 및 개발 업무",
-        ],
-        code: "",
-        view: "임금 체불로 퇴사( 퇴사 2년만에 수령 )",
-        info: [
-            "Flutter",
-            "Android",
-            "iOS",
-            "C++",
-        ],
-    },
-    {
-        yearmonth: "2019.03 - 2020.03",
-        text: ["Castflip", "개발총괄", "팀장"],
-        title: [
-            "- 건반 ( 동물 영양제 비교/합산 서비스 앱 - Android ) 개발",
-            "- 동물 용품 관련 종합 쇼핑몰 기획",
-        ],
-        code: "",
-        view: "임금 체불로 퇴사",
-        info: [
-            "Android",
-            "PHP",
-            "MariaDB",
-            "AWS",
-        ],
-    },
-    {
-        yearmonth: "2017.07 - 2019.01",
-        text: ["아라드네트웍스", "연구소", "선임연구원"],
-        title: [
-            "- 윈도우 네트워크 에이전트 개발( MFC )",
-            "- 네트워크 커널 드라이버 개발( WDM )", 
-            "- 사업기획 및 전략 업무",
-            "- 정부 과제 관리 업무",
-        ],
-        code: "",
-        view: "",
-        info: [
-            "C++",
-            "C",
-        ],
-    },
-    {
-        yearmonth: "2017.04 - 2017.07",
-        text: ["개인외주", "프리랜서"],
-        title: [
-            "- 시리얼 키 관리 윈도우 프로그램 개발", 
-            "- 클라이언트 시리얼 승인 모듈 개발",
-        ],
-        code: "",
-        view: "",
-        info: [
-            "C++",
-            "MariaDB",
-        ],
-    },
-    {
-        yearmonth: "2017.01 - 2017.03",
-        text: ["보고넷", "프리랜서"],
-        title: [
-            "- 수자원인력공사 : 하수도 누수 체크 솔루션 프로그램",
-        ],
-        code: "",
-        view: "",
-        info: [
-            "C++",
-            "OpenCV",
-            "Google API",
-        ],
-    },
-    {
-        yearmonth: "2014.04 - 2015.09",
-        text: ["청호컴넷", "Core S/W 개발", "주임연구원"],
-        title: [
-            "- 사내 모델별 디바이스 제어 프로그램 개발",
-            "- 통합 SP 프로젝트",
-            "- 통합 AP 프로젝트",
-        ],
-        code: "",
-        view: "",
-        info: [
-            "C++",
-            "C",
-            "MFC",
-            "Flash",
-            "XML",
-        ],
-    },
-    {
-        yearmonth: "2012.08 - 2014.04",
-        text: ["기산전자", "인식 개발팀", "주임연구원"],
-        title: [
-            "- Newton / Newton Plus / K-500 별 인식 알고리즘 및 각 국가 프로그램 개발",
-            "- 해외 중앙은행 기기 입찰 성능 시현",
-            "- 바이어 엔지니어 교육( 담당지 : 파리, 벨기에, 쿠웨이트 )",
-        ],
-        code: "",
-        view: "",
-        info: [
-            "C",
-        ],
-    },
-    {
-        yearmonth: "2010.03 - 2012.08",
-        text: ["숭실대학교 대학원", "석사", "졸업"],
-        title: [
-            "- 학점 : 3.6 / 4.5",
-            "- 지역 : 서울",
-            "- 졸업논문 : 동작 카메라를 이용한 새 무리 제어",
-        ],
-        code: "",
-        view: "",
-        info: [
-            "MFC",
-            "C++",
-        ],
-    },
+  {
+    yearmonth: "2024.05 - 현재",
+    text: ["키인즈", "미디어 개발팀", "선임연구원"],
+    title: [
+      "- LG U+ 화물잇고-차주앱 (Flutter - iOS/Android 앱) : 5월 ~ 9월",
+      "- 미래엔 초코툰 관리자 웹 (NextJS - ReactJS 웹) : 10월 ~ 12월",
+      "- LG U+ 모아티비 웹(NextJS - ReactJS 웹) : 12월 ~ 현재",
+    ],
+    code: "",
+    view: "",
+    info: ["Flutter", "NextJS", "ReactJS", "Android", "iOS"],
+  },
+  {
+    yearmonth: "2023.05 - 2024.01",
+    text: ["후이즈시스템", "솔루션 개발팀", "팀장"],
+    title: [
+      "- G2Works / G2Talk 전체 시스템 설계",
+      "- G2Works(그룹웨어) ( Flutter 앱 - Android / iOS )",
+      "- G2Talk(메신저) ( Flutter 앱 - Android / iOS / MacOS / Windows )",
+      "- G2Wokrs / G2Talk API ( Java Spring Boot )",
+    ],
+    code: "",
+    view: "임금 체불로 퇴사",
+    info: [
+      "Flutter - Android/iOS/Windows/MacOS",
+      "Java Spring Boot",
+      "Spring Data JDBC",
+      "Rest api",
+      "Docker",
+    ],
+  },
+  {
+    yearmonth: "2022.10 - 2023.02",
+    text: ["TDI", "프리랜서"],
+    title: ["- 눈팅 : 여성 커뮤니티 ( Flutter 앱 - Android / iOS )"],
+    code: "",
+    view: "",
+    info: ["Flutter", "Android", "iOS", "FastLane"],
+  },
+  {
+    yearmonth: "2021.03 - 2022.10",
+    text: ["UpTo", "개발총괄"],
+    title: [
+      "- B2B 기업 홍보 및 연결 앱 ( Flutter 앱 - Android / iOS )",
+      "- API 서버 개발( Python Flask, MariaDB )",
+    ],
+    code: "",
+    view: "",
+    info: ["Flutter", "Android", "iOS", "Python Flask", "MariaDB", "Rest api"],
+  },
+  {
+    yearmonth: "2020.07 - 2021.02",
+    text: ["파낙토스", "S/W팀", "과장"],
+    title: [
+      "- Panaxtos Manager 블루투스 뇌파 수신 어플 개발",
+      "- 사내 기존 C++ 관련 프로그램 유지보수 및 개발 업무",
+    ],
+    code: "",
+    view: "임금 체불로 퇴사( 퇴사 2년만에 수령 )",
+    info: ["Flutter", "Android", "iOS", "C++"],
+  },
+  {
+    yearmonth: "2019.03 - 2020.03",
+    text: ["Castflip", "개발전담", "팀장"],
+    title: [
+      "- 건반 ( 동물 영양제 비교/합산 서비스 초기모델 앱 - Android ) 개발",
+      "- 동물 용품 관련 종합 쇼핑몰 기획",
+    ],
+    code: "",
+    view: "임금 체불로 퇴사",
+    info: ["Android", "PHP", "MariaDB", "AWS"],
+  },
+  {
+    yearmonth: "2017.07 - 2019.01",
+    text: ["아라드네트웍스", "연구소", "선임연구원"],
+    title: [
+      "- 윈도우 네트워크 에이전트 개발( MFC )",
+      "- 네트워크 커널 드라이버 개발( WDM )",
+      "- 사업기획 및 전략 업무",
+      "- 정부 과제 관리 업무",
+    ],
+    code: "",
+    view: "",
+    info: ["C++", "C"],
+  },
+  {
+    yearmonth: "2017.04 - 2017.07",
+    text: ["개인외주", "프리랜서"],
+    title: [
+      "- 시리얼 키 관리 윈도우 프로그램 개발",
+      "- 클라이언트 시리얼 승인 모듈 개발",
+    ],
+    code: "",
+    view: "",
+    info: ["C++", "MariaDB"],
+  },
+  {
+    yearmonth: "2017.01 - 2017.03",
+    text: ["보고넷", "프리랜서"],
+    title: ["- 수자원인력공사 : 하수도 누수 체크 솔루션 프로그램"],
+    code: "",
+    view: "",
+    info: ["C++", "OpenCV", "Google API"],
+  },
+  {
+    yearmonth: "2014.04 - 2015.09",
+    text: ["청호컴넷", "Core S/W 개발", "주임연구원"],
+    title: [
+      "- 사내 모델별 디바이스 제어 프로그램 개발",
+      "- 통합 SP 프로젝트",
+      "- 통합 AP 프로젝트",
+    ],
+    code: "",
+    view: "",
+    info: ["C++", "C", "MFC", "Flash", "XML"],
+  },
+  {
+    yearmonth: "2012.08 - 2014.04",
+    text: ["기산전자", "인식 개발팀", "주임연구원"],
+    title: [
+      "- Newton / Newton Plus / K-500 별 인식 알고리즘 및 각 국가 프로그램 개발",
+      "- 해외 중앙은행 기기 입찰 성능 시현",
+      "- 바이어 엔지니어 교육( 담당지 : 파리, 벨기에, 쿠웨이트 )",
+    ],
+    code: "",
+    view: "",
+    info: ["C"],
+  },
+  {
+    yearmonth: "2010.03 - 2012.08",
+    text: ["숭실대학교 대학원", "석사", "졸업"],
+    title: [
+      "- 학점 : 3.6 / 4.5",
+      "- 지역 : 서울",
+      "- 졸업논문 : 동작 카메라를 이용한 새 무리 제어",
+    ],
+    code: "",
+    view: "",
+    info: ["MFC", "C++"],
+  },
 ];
 
 export const summaryText = [
-    {
-        yearmonth: "키인즈 / 2024.05 - 현재",
-        title: "서비스개발팀 / 선임연구원",
-        desc: [
-            "- U+ 화물잇고-차주앱 (Flutter - iOS/Android 앱) : 5월 ~ 9월",
-            "- 미래엔 관리자 웹 (NextJS) : 10월 ~ 현재",
-        ],
-    },
-    {
-        yearmonth: "후이즈시스템 / 2023.05 - 2024.01",
-        title: "솔루션 개발팀 / 팀장",
-        desc: [
-            "- G2Works / G2Talk 전체 시스템 설계",
-            "- G2Works(그룹웨어) ( Flutter 앱 - Android / iOS )",
-            "- G2Talk(메신저) ( Flutter 앱 - Android / iOS / MacOS / Windows )",
-            "- G2Wokrs / G2Talk API ( Java Spring Boot )",
-            "",
-            "임금 체불로 퇴사"
-        ],
-    },
-    {
-        yearmonth: "TDI / 2022.10 - 2023.02",
-        title: "프리랜서",
-        desc: [
-            "- 눈팅 : 여성 커뮤니티 ( Flutter 앱 - Android / iOS )",
-        ],
-    },
-    {
-        yearmonth: "UpTo / 2021.03 - 2022.10",
-        title: "개발총괄",
-        desc: [
-            "- B2B 기업 홍보 및 연결 앱 ( Flutter 앱 - Android / iOS )", 
-            "- API 서버 개발( Python Flask, MariaDB )",
-        ],
-    },
-    {
-        yearmonth: "파낙토스 / 2020.07 - 2021.02",
-        title: "S/W팀 / 과장",
-        desc: [
-            "- Panaxtos Manager 블루투스 뇌파 수신 어플 개발",
-            "- 사내 기존 C++ 관련 프로그램 유지보수 및 개발 업무",
-            "",
-            "임금 체불로 퇴사( 퇴사 2년만에 수령 )",
-        ],
-    },
-    {
-        yearmonth: "Castflip / 2019.03 - 2020.03",
-        title: "개발총괄 / 팀장",
-        desc: [
-            "- 건반 ( 동물 영양제 비교/합산 서비스 앱 - Android ) 개발",
-            "- 동물 용품 관련 종합 쇼핑몰 기획",
-            "",
-            "임금 체불로 퇴사",
-        ],
-    },
-    {
-        yearmonth: "아라드네트웍스 / 2017.07 - 2019.01",
-        title: "연구소 / 선임연구원",
-        desc: [
-            "- 윈도우 네트워크 에이전트 개발( MFC )",
-            "- 네트워크 커널 드라이버 개발( WDM )", 
-            "- 사업기획 및 전략 업무",
-            "  자율협력주행 데이터 활용 공모전 기획 및 구조 설계 / 최우수상 수상",
-            "  벤처나라 제품 등록( SPN 솔루션 )",
-            "  NET/NEP 인증( SPN 솔루션 )",
-            "- 정부 과제 관리 업무",
-            "  차세대 무선 CCTV 용 IoT 보안 플랫폼 개발",
-            "  안전한 산업용 네트워크 인프라를 위한 소프트웨어정의 기반 IoT 네트워크 보안(SD-IoT Network Security) 시스템 개발",
-            "  안전한 공동주택 스마트홈 네트워크를 위한 세대간 사이버 경계벽 기술",
-            "  안전한 스마트도시를 위한 사이버 경계벽 IoT 보안 플랫폼 기술 개발",
-            "  초대규모(Massive) 실시간 IoT 인프라 및 네트워크 기술 개발",
-        ],
-    },
-    {
-        yearmonth: "개인외주 / 2017.04 - 2017.07",
-        title: "프리랜서",
-        desc: [
-            "- 시리얼 키 관리 윈도우 프로그램 개발", 
-            "- 클라이언트 시리얼 승인 모듈 개발",
-        ],
-    },
-    {
-        yearmonth: "보고넷 / 2017.01 - 2017.03",
-        title: "프리랜서",
-        desc: [
-            "- 수자원인력공사 : 하수도 누수 체크 솔루션 프로그램",
-        ],
-    },
-    {
-        yearmonth: "청호컴넷 / 2014.04 - 2015.09",
-        title: "Core S/W 개발 / 주임연구원",
-        desc: [
-            "- 사내 모델별 디바이스 제어 프로그램 개발",
-            "이란향 VDA",
-            "Intermac(Bacode)",
-            "호주향 OCX 플랫폼",
-            "TIT_DIP(Card Reader)",
-            "HANMEGA EPP(PIN Pad)",
-            "- 통합 SP 프로젝트",
-            "ZT598M(PIN Pad)",
-            "러시아향 VDA",
-            "ZT588F(PIN Pad)",
-            "EATON 5P-580(UPS)",
-            "- 통합 AP 프로젝트",
-            "신협용 EPP(PIN Pad)",
-        ],
-    },
-    {
-        yearmonth: "기산전자 / 2012.08 - 2014.04",
-        title: "인식 개발팀 / 주임연구원",
-        desc: [
-            "- 인식 알고리즘 및 각 국가 프로그램 개발",
-            "Newton",
-            "Newton Plus",
-            "K-500",
-            "- 해외 담당 업무( 담당지 : 파리, 벨기에, 쿠웨이트 )",
-            "중앙은행 기기 입찰 성능 시현",
-            "바이어 엔지니어 교육",
-        ],
-    },
-    {
-        yearmonth: "숭실대학교 대학원 / 2010.03 - 2012.08",
-        title: "석사 / 졸업",
-        desc: [
-            "- 학점 : 3.6 / 4.5",
-            "- 지역 : 서울",
-            "- 졸업논문 : 동작 카메라를 이용한 새 무리 제어",
-        ],
-    },
+  {
+    yearmonth: "키인즈 / 2024.05 - 현재",
+    title: "미디어개발팀 / 선임연구원",
+    desc: [
+      "- LG U+ 화물잇고-차주앱 (Flutter - iOS/Android 앱) : 5월 ~ 9월",
+      "- 미래엔 초코툰 관리자 웹 (NextJS - ReactJS 웹) : 10월 ~ 12월",
+      "- LG U+ 모아티비 웹(NextJS - ReactJS 웹) : 12월 ~ 현재",
+      "",
+    ],
+  },
+  {
+    yearmonth: "후이즈시스템 / 2023.05 - 2024.01",
+    title: "솔루션 개발팀 / 팀장",
+    desc: [
+      "- G2Works / G2Talk 전체 시스템 설계",
+      "- G2Works(그룹웨어) ( Flutter 앱 - Android / iOS )",
+      "- G2Talk(메신저) ( Flutter 앱 - Android / iOS / MacOS / Windows )",
+      "- G2Wokrs / G2Talk API ( Java Spring Boot )",
+      "",
+      "임금 체불로 퇴사",
+      "",
+    ],
+  },
+  {
+    yearmonth: "TDI / 2022.10 - 2023.02",
+    title: "프리랜서",
+    desc: ["- 눈팅 : 여성 커뮤니티 ( Flutter 앱 - Android / iOS )", ""],
+  },
+  {
+    yearmonth: "UpTo / 2021.03 - 2022.10",
+    title: "개발총괄",
+    desc: [
+      "- B2B 기업 홍보 및 연결 앱 ( Flutter 앱 - Android / iOS )",
+      "- API 서버 개발( Python Flask, MariaDB )",
+      "",
+    ],
+  },
+  {
+    yearmonth: "파낙토스 / 2020.07 - 2021.02",
+    title: "S/W팀 / 과장",
+    desc: [
+      "- Panaxtos Manager 블루투스 뇌파 수신 어플 개발",
+      "- 사내 기존 C++ 관련 프로그램 유지보수 및 개발 업무",
+      "",
+      "임금 체불로 퇴사( 퇴사 2년만에 수령 )",
+      "",
+    ],
+  },
+  {
+    yearmonth: "Castflip / 2019.03 - 2020.03",
+    title: "개발전담 / 팀장",
+    desc: [
+      "- 건반 ( 동물 영양제 비교/합산 서비스 초기 모델 앱 - Android ) 개발",
+      "- 동물 용품 관련 종합 쇼핑몰 기획",
+      "",
+      "임금 체불로 퇴사",
+      "",
+    ],
+  },
+  {
+    yearmonth: "아라드네트웍스 / 2017.07 - 2019.01",
+    title: "연구소 / 선임연구원",
+    desc: [
+      "- 윈도우 네트워크 에이전트 개발( MFC )",
+      "- 네트워크 커널 드라이버 개발( WDM )",
+      "- 사업기획 및 전략 업무",
+      "  자율협력주행 데이터 활용 공모전 기획 및 구조 설계 / 최우수상 수상",
+      "  벤처나라 제품 등록( SPN 솔루션 )",
+      "  NET/NEP 인증( SPN 솔루션 )",
+      "- 정부 과제 관리 업무",
+      "  차세대 무선 CCTV 용 IoT 보안 플랫폼 개발",
+      "  안전한 산업용 네트워크 인프라를 위한 소프트웨어정의 기반 IoT 네트워크 보안(SD-IoT Network Security) 시스템 개발",
+      "  안전한 공동주택 스마트홈 네트워크를 위한 세대간 사이버 경계벽 기술",
+      "  안전한 스마트도시를 위한 사이버 경계벽 IoT 보안 플랫폼 기술 개발",
+      "  초대규모(Massive) 실시간 IoT 인프라 및 네트워크 기술 개발",
+      "",
+    ],
+  },
+  {
+    yearmonth: "개인외주 / 2017.04 - 2017.07",
+    title: "프리랜서",
+    desc: [
+      "- 시리얼 키 관리 윈도우 프로그램 개발",
+      "- 클라이언트 시리얼 승인 모듈 개발",
+      "",
+    ],
+  },
+  {
+    yearmonth: "보고넷 / 2017.01 - 2017.03",
+    title: "프리랜서",
+    desc: ["- 수자원인력공사 : 하수도 누수 체크 솔루션 프로그램", ""],
+  },
+  {
+    yearmonth: "청호컴넷 / 2014.04 - 2015.09",
+    title: "Core S/W 개발 / 주임연구원",
+    desc: [
+      "- 사내 모델별 디바이스 제어 프로그램 개발",
+      "이란향 VDA",
+      "Intermac(Bacode)",
+      "호주향 OCX 플랫폼",
+      "TIT_DIP(Card Reader)",
+      "HANMEGA EPP(PIN Pad)",
+      "- 통합 SP 프로젝트",
+      "ZT598M(PIN Pad)",
+      "러시아향 VDA",
+      "ZT588F(PIN Pad)",
+      "EATON 5P-580(UPS)",
+      "- 통합 AP 프로젝트",
+      "신협용 EPP(PIN Pad)",
+      "",
+    ],
+  },
+  {
+    yearmonth: "기산전자 / 2012.08 - 2014.04",
+    title: "인식 개발팀 / 주임연구원",
+    desc: [
+      "- 인식 알고리즘 및 각 국가 프로그램 개발",
+      "Newton",
+      "Newton Plus",
+      "K-500",
+      "- 해외 담당 업무( 담당지 : 파리, 벨기에, 쿠웨이트 )",
+      "중앙은행 기기 입찰 성능 시현",
+      "바이어 엔지니어 교육",
+      "",
+    ],
+  },
+  {
+    yearmonth: "숭실대학교 대학원 / 2010.03 - 2012.08",
+    title: "석사 / 졸업",
+    desc: [
+      "- 학점 : 3.6 / 4.5",
+      "- 지역 : 서울",
+      "- 졸업논문 : 동작 카메라를 이용한 새 무리 제어",
+      "",
+    ],
+  },
 ];
 
 export const footerText = [
-    {
-        title: "사람인",
-        desc: "이력서/경력기술서/포트폴리오 다운로드 가능합니다.",
-        link: "https://www.saramin.co.kr/",
-    },
-    {
-        title: "잡코리아",
-        desc: "이력서/경력기술서/포트폴리오 다운로드 가능합니다.",
-        link: "https://www.jobkorea.co.kr/",
-    },
-    
+  {
+    title: "사람인",
+    desc: "이력서/경력기술서/포트폴리오 다운로드 가능합니다.",
+    link: "https://www.saramin.co.kr/",
+  },
+  {
+    title: "잡코리아",
+    desc: "이력서/경력기술서/포트폴리오 다운로드 가능합니다.",
+    link: "https://www.jobkorea.co.kr/",
+  },
 ];
 
 export const contactText = [
-    {
-        link: "mailto:work.hethan@gmail.com",
-        title: "Mail : work.hethan@gmail.com",
-    },
+  {
+    link: "mailto:work.hethan@gmail.com",
+    title: "Mail : work.hethan@gmail.com",
+  },
 ];
